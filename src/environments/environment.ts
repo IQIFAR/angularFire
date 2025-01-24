071778a145f5107d1073f5df2296e3f7a3952e17
IQIFAR <2023410832@student.uitm.edu.my>
2025-01-24 18:36:47 +0800

    2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
+    apiKey: "AIzaSyCzDUhsAhROHWisr8XeXkOymMbN17V5zQY",
+    authDomain: "friendlychat-dfd70.firebaseapp.com",
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
+    projectId: "friendlychat-dfd70",
+    storageBucket: "friendlychat-dfd70.firebasestorage.app",
+    messagingSenderId: "1057392445922",
+    appId: "1:1057392445922:web:27291fffa2df8be01591bb",
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
